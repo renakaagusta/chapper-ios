585706b7507cb575dc361576d3215a6501403dc6
renakaagusta <renakaagusta28@gmail.com>
2022-10-21 17:02:15 +0700

    Add reset camera function

--- Chapper/Component/Atom/Progress Bar/ProgressBar.swift
+++ Chapper/Component/Atom/Progress Bar/ProgressBar.swift
@@ -8,29 +8,25 @@
 import SwiftUI
 
 struct AppProgressBar: View {
-    
     var width: CGFloat = 300
     var height: CGFloat = 7
-    var percent: CGFloat = 69
+    @Binding var progress: CGFloat
     
     var body: some View {
-        let multiplier = width / 100
-        
         ZStack(alignment: .leading) {
             RoundedRectangle(cornerRadius: 17, style: .continuous)
                 .frame(width: width, height: height)
-            .foregroundColor(Color.foot.primary.opacity(0.5))
+            .foregroundColor(Color.foot.primary.opacity(0.3))
             
             RoundedRectangle(cornerRadius: 17, style: .continuous)
-                .frame(width: percent * multiplier, height: height)
-            .foregroundColor(Color.foot.primary)
-        }
-        
+                .frame(width: width * (progress > 1 ? 1 : progress), height: height)
+                .foregroundColor(Color.foot.primary)
+            }
     }
 }
-
-struct AppProgressBar_Previews: PreviewProvider {
-    static var previews: some View {
-        AppProgressBar()
-    }
-}
+//
+//struct AppProgressBar_Previews: PreviewProvider {
+//    static var previews: some View {
+//        AppProgressBar()
+//    }
+//}
--- Chapper/View/StoryList/StoryListView.swift
+++ Chapper/View/StoryList/StoryListView.swift
@@ -10,37 +10,46 @@ import SwiftUI
 struct StoryListView: View {
     
     var storyList = [
-        StoryData(
-            id: "1",
-            title: "Story 1",
-            description: "Description 1",
-            thumbnail: "",
-            sceneName: "3DAssetS1",
-            objectList: [
-                ObjectScene(
-                    title: "Object 1",
-                    description: "Description 1",
-                    hint: "Hint 1",
-                    tag: "Cube_001"
-                )
-            ]
-        ),
-        StoryData(
-            id: "2",
-            title: "Story 2",
-            description: "Description 1",
-            thumbnail: "",
-            sceneName: "3DAssetS1",
-            objectList: [
-                ObjectScene(
-                    title: "Object 1",
-                    description: "Description 1",
-                    hint: "Hint 1",
-                    tag: "Cube_001"
-                )
-            ]
-        )
-    ]
+           StoryData(
+               id: "1",
+               title: "Story 1",
+               description: "Description 1",
+               thumbnail: "",
+               sceneName: "3DAssetS1",
+               sceneExtension: "scn",
+               objectList: [
+                   ObjectScene(
+                       title: "Object 1",
+                       description: "Description 1",
+                       hint: "Hint 1",
+                       tag: "Cube_001",
+                       narationDuration: 15,
+                       instructionList: [
+                           Instruction(id: "1", text: "Narasi 1", startedAt: 0),
+                           Instruction(id: "1",text: "Narasi 2", startedAt: 5),
+                           Instruction(id: "1",text: "Narasi 3", startedAt: 10)
+                       ]
+                   )
+               ]
+           ),
+           StoryData(
+               id: "2",
+               title: "Story 2",
+               description: "Description 1",
+               thumbnail: "",
+               sceneName: "3DAssetS1",
+               sceneExtension: "scn",
+               objectList: [
+                   ObjectScene(
+                       title: "Object 1",
+                       description: "Description 1",
+                       hint: "Hint 1",
+                       tag: "Cube_001",
+                       narationDuration: 15
+                   )
+               ]
+           )
+       ]
     
     var body: some View {
         VStack {
